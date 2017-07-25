417665ebec5466865017f6df1a72c4c5f95817c5
Jeffrey Gelens <jeffrey@noppo.pro>
2017-07-25 16:04:33 +0200

    migrated to py3

--- lambda-ebs-backup-cleanup.py
+++ lambda-ebs-backup-cleanup.py
@@ -3,29 +3,30 @@ import re
 import datetime
 import base64
 import os
-import json
 
-base64_region = os.environ['aws_regions']
-iam = boto3.client('iam')
-aws_sns_arn = os.getenv('aws_sns_arn', None)
+base64_region = os.environ["aws_regions"]
+iam = boto3.client("iam")
+aws_sns_arn = os.getenv("aws_sns_arn", None)
+
 
 def send_to_sns(subject, message):
     if aws_sns_arn is None:
         return
 
-    print "Sending notification to: %s" % aws_sns_arn
+    print("Sending notification to: %s" % aws_sns_arn)
 
-    client = boto3.client('sns')
+    client = boto3.client("sns")
 
     response = client.publish(
         TargetArn=aws_sns_arn,
         Message=message,
         Subject=subject)
 
-    if 'MessageId' in response:
-        print "Notification sent with message id: %s" % response['MessageId']
+    if "MessageId" in response:
+        print("Notification sent with message id: %s" % response["MessageId"])
     else:
-        print "Sending notification failed with response: %s" % str(response)
+        print("Sending notification failed with response: %s" % str(response))
+
 
 """
 This function looks at *all* snapshots that have a "DeleteOn" tag containing
@@ -33,44 +34,48 @@ the current day formatted as YYYY-MM-DD. This function should be run at least
 daily.
 """
 
+
 def lambda_handler(event, context):
     decoded_regions = base64.b64decode(base64_region)
-    regions = decoded_regions.split(',')
+    regions = decoded_regions.decode().split(",")
 
-    print "Cleaning up snapshots in regions: %s" % regions
+    print("Cleaning up snapshots in regions: %s" % regions)
 
     for region in regions:
-        ec = boto3.client('ec2', region_name=region)
+        ec = boto3.client("ec2", region_name=region)
         account_ids = list()
         try:
             """
             You can replace this try/except by filling in `account_ids` yourself.
             Get your account ID with:
             > import boto3
-            > iam = boto3.client('iam')
-            > print iam.get_user()['User']['Arn'].split(':')[4]
+            > iam = boto3.client("iam")
+            > print iam.get_user()["User"]["Arn"].split(":")[4]
             """
             iam.get_user()
         except Exception as e:
-            # use the exception message to get the account ID the function executes under
-            account_ids.append(re.search(r'(arn:aws:sts::)([0-9]+)', str(e)).groups()[1])
+            # use the exception message to get the account ID the function
+            # executes under
+            account_ids.append(
+                re.search(r"(arn:aws:sts::)([0-9]+)", str(e)).groups()[1])
 
-
-        delete_on = datetime.date.today().strftime('%Y-%m-%d')
+        delete_on = datetime.date.today().strftime("%Y-%m-%d")
         filters = [
-            {'Name': 'tag-key', 'Values': ['DeleteOn']},
-            {'Name': 'tag-value', 'Values': [delete_on]},
+            {"Name": "tag-key", "Values": ["DeleteOn"]},
+            {"Name": "tag-value", "Values": [delete_on]},
         ]
-        snapshot_response = ec.describe_snapshots(OwnerIds=account_ids, Filters=filters)
+        snapshot_response = ec.describe_snapshots(
+            OwnerIds=account_ids, Filters=filters)
 
-        print "Found %d snapshots that need deleting in region %s on %s" % (
-            len(snapshot_response['Snapshots']),
+        print("Found %d snapshots that need deleting in region %s on %s" % (
+            len(snapshot_response["Snapshots"]),
             region,
-            delete_on)
+            delete_on))
 
-        for snap in snapshot_response['Snapshots']:
-            print "Deleting snapshot %s" % snap['SnapshotId']
-            ec.delete_snapshot(SnapshotId=snap['SnapshotId'])
+        for snap in snapshot_response["Snapshots"]:
+            print("Deleting snapshot %s" % snap["SnapshotId"])
+            ec.delete_snapshot(SnapshotId=snap["SnapshotId"])
 
-        message = "{} snapshots have been cleaned up in region {}".format(len(snapshot_response['Snapshots']), region)
-        send_to_sns('EBS Backups Cleanup', message)
+        message = ("{} snapshots have been cleaned up in "
+                   "region {}").format(len(snapshot_response["Snapshots"]), region)
+        send_to_sns("EBS Backups Cleanup", message)
--- lambda-ebs-backup.py
+++ lambda-ebs-backup.py
@@ -3,53 +3,55 @@ import collections
 import datetime
 import base64
 import os
-import json
-import itertools
 
-base64_region = os.environ['aws_regions']
-aws_sns_arn = os.getenv('aws_sns_arn', None)
+
+base64_region = os.environ["aws_regions"]
+aws_sns_arn = os.getenv("aws_sns_arn", None)
+
 
 def send_to_sns(subject, message):
     if aws_sns_arn is None:
         return
 
-    print "Sending notification to: %s" % aws_sns_arn
+    print("Sending notification to: %s" % aws_sns_arn)
 
-    client = boto3.client('sns')
+    client = boto3.client("sns")
 
     response = client.publish(
         TargetArn=aws_sns_arn,
         Message=message,
         Subject=subject)
 
-    if 'MessageId' in response:
-        print "Notification sent with message id: %s" % response['MessageId']
+    if "MessageId" in response:
+        print("Notification sent with message id: %s" % response["MessageId"])
     else:
-        print "Sending notification failed with response: %s" % str(response)
+        print("Sending notification failed with response: %s" % str(response))
+
 
 def lambda_handler(event, context):
     decoded_regions = base64.b64decode(base64_region)
-    regions = decoded_regions.split(',')
+    regions = decoded_regions.decode().split(",")
 
-    print "Backing up instances in regions: %s" % regions
+    print("Backing up instances in regions: %s" % regions)
 
     for region in regions:
-        ec = boto3.client('ec2', region_name=region)
+        ec = boto3.client("ec2", region_name=region)
         reservations = ec.describe_instances(
             Filters=[
-                {'Name': 'tag-key', 'Values': ['backup', 'Backup']},
+                {"Name": "tag-key", "Values": ["backup", "Backup"]},
             ]
         ).get(
-            'Reservations', []
+            "Reservations", []
         )
 
         instances = sum(
             [
-                [i for i in r['Instances']]
+                [i for i in r["Instances"]]
                 for r in reservations
             ], [])
 
-        print "Found %d instances that need backing up in region %s" % (len(instances), region)
+        print("Found %d instances that need backing up in "
+              "region %s" % (len(instances), region))
 
         to_tag_retention = collections.defaultdict(list)
         to_tag_mount_point = collections.defaultdict(list)
@@ -57,65 +59,74 @@ def lambda_handler(event, context):
         for instance in instances:
             try:
                 retention_days = [
-                    int(t.get('Value')) for t in instance['Tags']
-                    if t['Key'] == 'Retention'][0]
+                    int(t.get("Value")) for t in instance["Tags"]
+                    if t["Key"] == "Retention"][0]
             except IndexError:
                 retention_days = 7
 
             try:
                 skip_volumes = [
-                    str(t.get('Value')).split(',') for t in instance['Tags']
-                    if t['Key'] == 'Skip_Backup_Volumes']
+                    str(t.get("Value")).split(",") for t in instance["Tags"]
+                    if t["Key"] == "Skip_Backup_Volumes"]
             except Exception:
                 pass
 
             from itertools import chain
             skip_volumes_list = list(chain.from_iterable(skip_volumes))
 
-            for dev in instance['BlockDeviceMappings']:
-                if dev.get('Ebs', None) is None:
+            for dev in instance["BlockDeviceMappings"]:
+                if dev.get("Ebs", None) is None:
                     continue
-                vol_id = dev['Ebs']['VolumeId']
+
+                vol_id = dev["Ebs"]["VolumeId"]
+
                 if vol_id in skip_volumes_list:
-                    print "Volume %s is set to be skipped, not backing up" % (vol_id)
+                    print("Volume %s is set to be skipped,"
+                          " not backing up" % (vol_id))
                     continue
-                dev_attachment = dev['DeviceName']
-                print "Found EBS volume %s on instance %s attached to %s" % (
-                    vol_id, instance['InstanceId'], dev_attachment)
+
+                dev_attachment = dev["DeviceName"]
+                print("Found EBS volume %s on instance %s attached to %s" % (
+                    vol_id, instance["InstanceId"], dev_attachment))
 
                 snap = ec.create_snapshot(
                     VolumeId=vol_id,
-                    Description=instance['InstanceId'],
+                    Description=instance["InstanceId"],
                 )
 
-                to_tag_retention[retention_days].append(snap['SnapshotId'])
-                to_tag_mount_point[vol_id].append(snap['SnapshotId'])
+                to_tag_retention[retention_days].append(snap["SnapshotId"])
+                to_tag_mount_point[vol_id].append(snap["SnapshotId"])
 
-
-                print "Retaining snapshot %s of volume %s from instance %s for %d days" % (
-                    snap['SnapshotId'],
-                    vol_id,
-                    instance['InstanceId'],
-                    retention_days,
-                )
+                print("Retaining snapshot %s of volume %s from "
+                      "instance %s for %d days" % (
+                          snap["SnapshotId"],
+                          vol_id,
+                          instance["InstanceId"],
+                          retention_days
+                      ))
 
                 ec.create_tags(
                     Resources=to_tag_mount_point[vol_id],
                     Tags=[
-                        {'Key': 'Name', 'Value': dev_attachment},
+                        {"Key": "Name", "Value": dev_attachment},
                     ]
                 )
 
-        for retention_days in to_tag_retention.keys():
-            delete_date = datetime.date.today() + datetime.timedelta(days=retention_days)
-            delete_fmt = delete_date.strftime('%Y-%m-%d')
-            print "Will delete %d snapshots on %s" % (len(to_tag_retention[retention_days]), delete_fmt)
+        for retention_days in list(to_tag_retention.keys()):
+            delete_date = datetime.date.today() + datetime.timedelta(
+                days=retention_days)
+            delete_fmt = delete_date.strftime("%Y-%m-%d")
+
+            print("Will delete %d snapshots on %s" % (
+                len(to_tag_retention[retention_days]), delete_fmt))
+
             ec.create_tags(
                 Resources=to_tag_retention[retention_days],
                 Tags=[
-                    {'Key': 'DeleteOn', 'Value': delete_fmt},
+                    {"Key": "DeleteOn", "Value": delete_fmt},
                 ]
             )
 
-        message = "{} instances have been backed up in region {}".format(len(instances), region)
-        send_to_sns('EBS Backups', message)
+        message = ("{} instances have been backed up in "
+                   "region {}").format(len(instances), region)
+        send_to_sns("EBS Backups", message)
